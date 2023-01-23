04d670f4410ac2ae5756f2a2da15ac4f5fa5e37b
butka <Bda.67@yandex.ru>
2023-01-23 17:01:51 +0300

    again

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,16 +6,17 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
     next(new AuthError('Вы не авторизированы'));
-  } else {
-    const token = req.cookies.jwt;
-    let playload;
-
-    try {
-      playload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
-      req.users = playload;
-      next();
-    } catch (err) {
-      next(new AuthError('Вы не авторизированы'));
-    }
   }
+  const token = req.cookies.jwt;
+  let playload;
+
+  try {
+    playload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    req.users = playload;
+    next();
+  } catch (err) {
+    next(new AuthError('Вы не авторизированы'));
+  }
+  req.user = playload;
+  return next();
 };
