01755fff1b97ac9dbe98754ef1e3dc849ca0da34
butka <Bda.67@yandex.ru>
2023-01-26 17:27:13 +0300

    fix1

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const { AuthError } = require('../errors/AllErrors');
 
 const userSchema = new mongoose.Schema({
